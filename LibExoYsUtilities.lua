d21c1369d7ede91737f307279931f1b700f8b41d
ExoY94 <exoy@mail.de>
2022-11-17 01:58:02 +0100

    Update LibExoYsUtilities.lua
    Color Setup

--- LibExoYsUtilities.lua
+++ LibExoYsUtilities.lua
@@ -5,10 +5,32 @@ local EM = GetEventManager()
 
 Lib.name = "LibExoYsUtilities"
 
+-------------------------------
+-- Player Initial Activation --
+-------------------------------
+-- executes callbacks for the first "PLAYER_ACTIVATED" event after an reloadui/login
+-- exposed function to allow for other modules/addons to register callbacks
+-- unregister function would be kinda pointless
+
+local initialActivationCallbackList = {}
+
+-- exposed function
+function Lib.RegisterInitialActivationCallback(callback)
+    table.insert(initialActivationCallbackList, callback)
+end
+
+function OnInitialActivation()
+    for _, callback in ipairs( initialActivationCallbackList ) do
+        if type(callback) = "function" then 
+            callback()
+        end
+    end
+    EM:UnregisterForEvent(Lib.name.."InitialActivation", EVENT_PLAYER_ACTIVATED)
+end
 
 --------------------
 -- Initialization --
---------------------
+-------------------- 
 
 local function InitModule(module) 
     local func = Lib[module.."_InitFunc"] or nil 
@@ -16,15 +38,17 @@ local function InitModule(module)
         func() 
     end
     func = nil 
-end
+end 
 
 local function Initialize()
     
+    EM:RegisterForEvent(Lib.name.."InitialActivation", EVENT_PLAYER_ACTIVATED, OnInitialActivation)
+
     InitModule("CombatStateManager")
 
 end
-  
-  
+
+
 local function OnAddonLoaded(_, addonName)
     if addonName == Lib.name then
         EM:UnregisterForEvent(Lib.name, EVENT_ADD_ON_LOADED)
